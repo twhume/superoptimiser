9a7a9b2ead10e4c9fe1eda61d685406daf941734
Tom Hume <twhume@gmail.com>
2012-05-14 16:50:15 +0100

    Added unit tests for ordering of test-map and incrementing of failure counts

--- Scratchpad/src/Scratchpad/test-map.clj
+++ Scratchpad/src/Scratchpad/test-map.clj
@@ -13,6 +13,20 @@
 ; 2. To order the running of equivalence tests on candidate classes
 ;
 
+; These two trivial functions are used for our unit tests
+
+(defn no-As?
+  "Predicate that returns true if the supplied sequence doesn't include A"
+  [l]
+  (not (some #(= \A %) l))
+)
+
+(defn no-Bs?
+  "Predicate that returns true if the supplied sequence doesn't include B"
+  [l]
+  (not (some #(= \B %) l))
+)
+
 ; As we use Test Maps in a couple of places, each one must keep track of its own list of
 ; test functions. When a Test Map is first initialised, we build a map from the list of 
 ; functions passed in, and initialise the failure count for each function to 0.
@@ -29,6 +43,33 @@
   (swap! tm conj [f (+ (@tm f) 1)])
 )
 
+; unit tests for inc-fail-count
+
+(let [tm (test-map [no-As? no-Bs?]) a-test (key (first @tm)) b-test (key (last @tm))]
+  
+  ; Increment a-test and check that the values are correct
+  
+  (inc-fail-count tm a-test)
+  (is (= 1 (@tm a-test)))
+  (is (= 0 (@tm b-test)))
+  (inc-fail-count tm a-test)
+  (is (= 2 (@tm a-test)))
+  (is (= 0 (@tm b-test)))
+  
+  ; Check the ordering is b-test then a-test
+  
+  (is (= b-test (key (first @tm))))
+  
+  ; Increment b-test until it's larger than a-test; check values and ordering
+  
+  (inc-fail-count tm b-test)
+  (inc-fail-count tm b-test)
+  (inc-fail-count tm b-test)
+  (is (= 2 (@tm a-test)))
+  (is (= 3 (@tm b-test)))
+  
+  (is (= a-test (key (first @tm)))))
+
 
 ;TODO this should increment the fail count for the first failure
 (defn passes?
@@ -36,33 +77,19 @@
   [tm s]
   (every? true? ((apply juxt (reverse (keys @tm))) s)))
 
+; unit tests for passes? function
 
-; These two trivial functions are used for our unit tests
+(let [t (test-map [no-As? no-Bs?])] 
+  (is (= true (passes? t '[\E \F \G])))
+  (is (= true (passes? t '[])))
 
-(defn no-As?
-  "Predicate that returns true if the supplied sequence doesn't include A"
-  [l]
-  (not (some #(= \A %) l))
-)
+	(is (= false (passes? t '[\A \B \C \D \E])))
+	(is (= false (passes? t '[\A])))
+	(is (= false (passes? t '[\B])))
+	(is (= false (passes? t '[\C \D \A \E])))
+	(is (= false (passes? t '[\C \D \E \B]))))
 
-(defn no-Bs?
-  "Predicate that returns true if the supplied sequence doesn't include B"
-  [l]
-  (not (some #(= \B %) l))
-)
 
-(def t (test-map [no-As? no-Bs?]))
-
-(is (= true (passes? t '[\E \F \G])))
-(is (= true (passes? t '[])))
-
-(is (= false (passes? t '[\A \B \C \D \E])))
-(is (= false (passes? t '[\A])))
-(is (= false (passes? t '[\B])))
-(is (= false (passes? t '[\C \D \A \E])))
-(is (= false (passes? t '[\C \D \E \B])))
-
-; Tidy up the namespace after all those tests
+; we only want those sample predicates for unit tests, so clear them out of our namespace
 (ns-unmap 'MessingAbout.test-map 'no-As?)
 (ns-unmap 'MessingAbout.test-map 'no-Bs?)
-(ns-unmap 'MessingAbout.test-map 't)
