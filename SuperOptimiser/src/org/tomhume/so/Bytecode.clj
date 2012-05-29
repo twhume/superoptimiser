73391dc4f5779ad4cb213744b72a2f9570781f33
Tom Hume <twhume@gmail.com>
2012-05-29 17:33:44 +0100

    Working loop in get-instructions

--- SuperOptimiser/src/org/tomhume/so/Bytecode.clj
+++ SuperOptimiser/src/org/tomhume/so/Bytecode.clj
@@ -8,23 +8,30 @@
 (defn get-instructions
   "Turns the supplied list of opcodes and arguments into an InsnList"
   [a]
-  nil
-)
+  (let [l (new InsnList)]
+    (loop [codes a]
+      (if (empty? codes) l
+        (let [op (first codes)]
+          (println op)
+          (recur (rest codes)))))))
 
 (defn get-class-bytes
   "Creates a Java Class from the supplied data, returns an array of bytes representing that class. Input should be a map containing keys
    :length, :vars and :code, containing the number of opcodes, the max. number of local variables and a list of opcodes and arguments"
+
   [code className methodName methodSig]
-  (let [cn (new ClassNode) cw (new ClassWriter ClassWriter/COMPUTE_MAXS) mn (new MethodNode (+ Opcodes/ACC_PUBLIC Opcodes/ACC_STATIC) methodName methodSig nil nil) ins (get-instructions code)]
+  
+  (let [cn (new ClassNode)
+        cw (new ClassWriter ClassWriter/COMPUTE_MAXS)
+        mn (new MethodNode (+ Opcodes/ACC_PUBLIC Opcodes/ACC_STATIC) methodName methodSig nil nil)
+        ins (get-instructions code)]
+    
     (set! (. cn version) Opcodes/V1_5)
     (set! (. cn access) Opcodes/ACC_PUBLIC)
     (set! (. cn name) className)
     (set! (. cn superName) "java/lang/Object")
     
-    ; make an InsnList out of the code passed in
-    
-  
-;    (doto (. mn instructions) (.add ins))
+    (doto (. mn instructions) (.add ins))
     (doto (. cn methods) (.add mn))
     (. cn accept cw)
     (. cw toByteArray)
@@ -37,4 +44,5 @@
     (with-open [out (FileOutputStream. fn)]
       (.write out b)))
 
-(write-bytes "/tmp/Identity.class" (get-class-bytes {} "IdentityTest" "identity" "(I)I"))
+;(write-bytes "/tmp/Identity.class" (get-class-bytes {} "IdentityTest" "identity" "(I)I"))
+(get-instructions '(:one :two :three))
