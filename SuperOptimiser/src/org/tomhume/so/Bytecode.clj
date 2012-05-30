e022c4987056a3de6186b3c3fe0471aa7f13374a
Tom Hume <twhume@gmail.com>
2012-05-30 09:05:36 +0100

    Can generate the identity function from Clojure

--- SuperOptimiser/src/org/tomhume/so/Bytecode.clj
+++ SuperOptimiser/src/org/tomhume/so/Bytecode.clj
@@ -1,18 +1,29 @@
 (ns org.tomhume.so.Bytecode)
+(import '(org.tomhume.so.Opcodes))
 (import '(java.io FileOutputStream))
 (import '(org.objectweb.asm ClassWriter Opcodes))
-(import '(org.objectweb.asm.tree AbstractInsnNode ClassNode MethodNode InsnList))
+(import '(org.objectweb.asm.tree AbstractInsnNode VarInsnNode InsnNode ClassNode MethodNode InsnList))
 
 ; This package handles the creation of Java class files.
 
 (defn add-opcode
+  "Creates a child of an AbstractInsNode and returns it"
+  [op & args]
+  (cond
+    (nil? ((opcodes op) :args)) (new InsnNode ((opcodes op) :opcode))
+    (= :istore op) (new VarInsnNode ((opcodes op) :opcode) (first args))
+    (= :iload op) (new VarInsnNode ((opcodes op) :opcode) (first args))
+    :else nil))
+
+(defn add-opcode-and-args
   "Pulls an opcode off the sequence provided, adds it and any arguments to the insnlist, returns the remainder of the sequence"
   [insnlist opcodes]
   (let [op (first opcodes)]
     (case op
-      :pop (do (println "pop!") (. insnlist (rest opcodes))
-      :istore (do (println "istore!") (nthrest opcodes 2))
-      :ireturn (do (println "return!") (rest opcodes))
+      :pop (do (println "pop!") (. insnlist add (add-opcode :pop)) (rest opcodes))
+      :ireturn (do (println "return!") (. insnlist add (add-opcode :ireturn)) (rest opcodes))
+      :istore (do (println "istore!") (. insnlist add (add-opcode :istore (second opcodes)))  (nthrest opcodes 2))
+      :iload (do (println "iload!") (. insnlist add (add-opcode :iload (second opcodes)))  (nthrest opcodes 2))
       ((println "unknown/in bad place!") (rest opcodes)))))
 
 (defn get-instructions
@@ -21,7 +32,7 @@
   (let [l (new InsnList)]
     (loop [codes a]
       (if (empty? codes) l
-        (recur (add-opcode l codes))))))
+        (recur (add-opcode-and-args l codes))))))
 
 (defn get-class-bytes
   "Creates a Java Class from the supplied data, returns an array of bytes representing that class. Input should be a map containing keys
@@ -42,9 +53,7 @@
     (doto (. mn instructions) (.add ins))
     (doto (. cn methods) (.add mn))
     (. cn accept cw)
-    (. cw toByteArray)
-  )
-)
+    (. cw toByteArray)))
 
 (defn write-bytes
   "Write a byte array to the filename specified"
@@ -52,5 +61,5 @@
     (with-open [out (FileOutputStream. fn)]
       (.write out b)))
 
-;(write-bytes "/tmp/Identity.class" (get-class-bytes {} "IdentityTest" "identity" "(I)I"))
-(get-instructions '(:pop :istore :arg :ireturn))
+(write-bytes "/tmp/Identity.class" (get-class-bytes '(:iload 0 :ireturn) "IdentityTest" "identity" "(I)I"))
+;(. (get-instructions '(:pop :istore 1 :ireturn)) size)
