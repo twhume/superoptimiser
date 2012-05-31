2f30618d9448be04cda1d7fcd0e58bcae470d7c3
Tom Hume <twhume@gmail.com>
2012-05-31 16:40:39 +0100

    Added in proper lein dependencies for ASM

--- SuperOptimiser/project.clj
+++ SuperOptimiser/project.clj
@@ -1,6 +1,8 @@
 (defproject SuperOptimiser "0.0.1"
   :description "Exhaustive searches for optimal JVM bytecode, while u wait"
-  :dependencies [[org.clojure/clojure "1.3.0"]
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [org.ow2.asm/asm "4.0"]
+                 [org.ow2.asm/asm-tree "4.0"]
                  [lein-eclipse "1.0.0"]
                  [org.clojure/math.combinatorics "0.0.2"]
                  [org.clojure/data.priority-map "0.0.1"]])
