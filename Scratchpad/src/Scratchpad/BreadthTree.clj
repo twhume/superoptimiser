d14b92911d68a9f8f44362717b2a9daeb438c854
Tom Hume <twhume@gmail.com>
2012-06-07 12:37:30 +0100

    Removed duplicate entries from list output

--- Scratchpad/src/Scratchpad/BreadthTree.clj
+++ Scratchpad/src/Scratchpad/BreadthTree.clj
@@ -1,22 +1,35 @@
 (ns Scratchpad.BreadthTree)
 
-(def atoms '(a b c))
-
-(defn ends-in-c?
-  [s]
-  (.endsWith s "c"))
+(def atoms '("a" "b" "c"))
 
 (defn no-double-a?
   [s]
-  (= -1 (.substring s "aa")))
+  (= -1 (.indexOf s "aa")))
 
+(defn no-abab?
+  [s]
+  (= -1 (.indexOf s "abab")))
 
-(defn get-children
-  [n]
-  (map #(str n %) atoms))
+(defn child-filter? [s] (and (no-double-a? s) (no-abab? s)))
+
+(defn get-children [n] (map #(str n %) atoms))
 
 (defn add-layer
   ([] (add-layer atoms))
   ([n] 
-    (let [children (flatten (map get-children n))]
-      (lazy-seq (concat n children ( add-layer children))))))
+    (let [child-nodes (filter child-filter? (flatten (map get-children n))) ]
+      (lazy-seq (concat n (add-layer child-nodes))))))
+
+; benchmarked at 190 entries/ms on EC2
+
+; GETTING DUPLICATES: (count (distinct (take 20 (add-layer)))) gives 12 ...
+
+
+; separate out pruning (dropping sections of the tree) from filtering (dropping individual items)
+
+
+(defn add-layer
+  ([] (add-layer atoms))
+  ([n] 
+    (let [child-nodes (flatten (map get-children n)) ]
+      (lazy-seq (concat n (add-layer child-nodes))))))
