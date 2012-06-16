dcf966cf94bf17c4cd3a11b5690eb011cee0f7da
Tom Hume <twhume@gmail.com>
2012-06-16 20:12:07 +0200

    Commented out some tests, so I get the same results I always used to...

--- SuperOptimiser/src/org/tomhume/so/SignumSO.clj
+++ SuperOptimiser/src/org/tomhume/so/SignumSO.clj
@@ -19,11 +19,11 @@
 (defn minus-one? [i]  (= -1 (invoke-method i method-name -1)))
 (defn is-zero? [i]  (= 0 (invoke-method i method-name 0)))
 (defn greater-than-one? [i]  (= 1 (invoke-method i method-name 999)))
-(defn greater-than-one-even? [i]  (= 1 (invoke-method i method-name 1000)))
+;(defn greater-than-one-even? [i]  (= 1 (invoke-method i method-name 1000)))
 (defn less-than-minus-one? [i]  (= -1 (invoke-method i method-name -999)))
-(defn less-than-minus-one? [i]  (= -1 (invoke-method i method-name -1000)))
+;(defn less-than-minus-one? [i]  (= -1 (invoke-method i method-name -1000)))
 
 
 (def eq-tests-filter (test-map [one? minus-one? is-zero? greater-than-one? less-than-minus-one?]))
 
-;(time (doall (superoptimise-pmap 4 class-name method-name method-signature eq-tests-filter)))
+(time (doall (superoptimise-pmap 4 class-name method-name method-signature eq-tests-filter)))
