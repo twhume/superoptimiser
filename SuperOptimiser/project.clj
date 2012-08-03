c0e19f42c9dc7441ebd5c55d6a9628ff1a86bcb9
Tom Hume <twhume@gmail.com>
2012-08-03 15:24:02 +0100

    Added logging stuffs to build path

--- SuperOptimiser/project.clj
+++ SuperOptimiser/project.clj
@@ -4,5 +4,6 @@
                  [org.ow2.asm/asm "4.0"]
                  [org.ow2.asm/asm-tree "4.0"]
                  [lein-eclipse "1.0.0"]
+                 [org.clojure/tools.logging "0.2.3"]
                  [org.clojure/math.combinatorics "0.0.2"]
                  [org.clojure/data.priority-map "0.0.1"]])
