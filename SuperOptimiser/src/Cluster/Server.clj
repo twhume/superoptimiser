0907647d9352bbd946d391f6f4d9ae42549dfefc
Tom Hume <twhume@gmail.com>
2012-08-06 15:47:06 +0100

    Server now suitable for running from leinengen

--- SuperOptimiser/src/Cluster/Server.clj
+++ SuperOptimiser/src/Cluster/Server.clj
@@ -2,17 +2,19 @@
   (:use noir.core)
   (:require [noir.server :as server]))
 
-(def maxnodes 10)
+(def server (server/start 8080))
 (def currentNode (atom -1))
-(def problem "Drivers.Negate")
 
 ; Used to start a server which hands out details of the number of nodes, node ID, and Clojure file to run
 ; Effectively, tells each server which part of the search space to handle
 
-(def server (server/start 8080))
 
-(defpage "/init" []
-  (let [next (swap! currentNode inc)]
+
+(defn -main [& args]
+  
+  (defpage "/init" []
+  (let [next (swap! currentNode inc)
+        maxnodes (Integer/parseInt (first args))
+        problem (second args)]
     (if (>= next maxnodes) "{}"
-      (str (hash-map :node next :max maxnodes :problem problem)))))
-
+      (str (hash-map :node next :max maxnodes :problem problem))))))
