36b639882f6b7fc4d93c62c90ebd3842e83184a8
Tom Hume <twhume@gmail.com>
2012-08-06 15:52:58 +0100

    Some additional imports

--- SuperOptimiser/project.clj
+++ SuperOptimiser/project.clj
@@ -5,6 +5,7 @@
                  [org.ow2.asm/asm-tree "4.0"]
                  [lein-eclipse "1.0.0"]
                  [noir "1.3.0-beta1"]
+                 [clj-http "0.5.2"]
                  [org.clojure/tools.logging "0.2.3"]
                  [org.clojure/math.combinatorics "0.0.2"]
                  [org.clojure/data.priority-map "0.0.1"]])
