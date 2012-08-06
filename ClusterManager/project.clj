8fb52a19f2fe525d6a5da70aef6453c728c858f1
Tom Hume <twhume@gmail.com>
2012-08-06 10:51:35 +0100

    First commit

---
+++ ClusterManager/project.clj
@@ -0,0 +1,4 @@
+(defproject ClusterManager "0.0.1"
+  :description "Manages the parallelisation of Superoptimiser jobs across many nodes"
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [lein-eclipse "1.0.0"]])
