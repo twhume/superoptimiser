df9070bbfb1a37fe8f3cd442d42cff078d8256cc
Tom Hume <twhume@gmail.com>
2012-08-06 16:02:13 +0100

    Client can now be run from leinengen

--- SuperOptimiser/src/Cluster/Client.clj
+++ SuperOptimiser/src/Cluster/Client.clj
@@ -2,21 +2,14 @@
   (:require [clj-http.client :as client])
   (:use [clojure.tools.logging :only (error)]))
 
-(let [endpoint "http://localhost:8080/init"
-      params (:body (client/get endpoint {:as :clojure}))]
-  (if (empty? params) (error "No parameters received back from server")
-    (do
-      (let [ns-sym (symbol (:problem params))
-            fn-sym (symbol (:problem params) "run-slice")]
-        (use ns-sym)
-        ((eval fn-sym) (:max params) (:node params))))))
-    
+(defn -main [& args]
+  (println args)
+	(let [endpoint (first args)
+	      params (:body (client/get endpoint {:as :clojure}))]
+	  (if (empty? params) (error "No parameters received back from server")
+	    (do
+	      (let [ns-sym (symbol (:problem params))
+	            fn-sym (symbol (:problem params) "run-slice")]
+	        (use ns-sym)
+	        ((eval fn-sym) (:max params) (:node params))))))) 
 
-
-; connect to endpoint
-; read the max-nodes, cur-node, problem
-; launch the problem with the associated parameters
-
-;(def maxnodes 10)
-;(def currentNode (atom -1))
-;(def problem "Drivers.Signum")
