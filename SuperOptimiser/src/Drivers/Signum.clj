c613577af4195d72deffc703b5bcf71d2871961e
Tom Hume <twhume@gmail.com>
2012-08-07 10:08:51 +0100

    Added some more tests

--- SuperOptimiser/src/Drivers/Signum.clj
+++ SuperOptimiser/src/Drivers/Signum.clj
@@ -9,9 +9,13 @@
       method-signature "(I)I"
       eq-tests-filter [
                        (fn greater-than-one-even? [i]  (= 1 (invoke-method i method-name 1000)))
+                       (fn greater-than-one-even2? [i]  (= 1 (invoke-method i method-name 2094)))
                        (fn less-than-minus-one? [i]  (= -1 (invoke-method i method-name -999)))
+                       (fn less-than-minus-one2? [i]  (= -1 (invoke-method i method-name -12458)))
                        (fn greater-than-one? [i]  (= 1 (invoke-method i method-name 999)))
+                       (fn greater-than-one2? [i]  (= 1 (invoke-method i method-name 9997)))
                        (fn less-than-minus-one-even? [i]  (= -1 (invoke-method i method-name -1000)))
+                       (fn less-than-minus-one-even2? [i]  (= -1 (invoke-method i method-name -12344)))
                        (fn one? [i]  (= 1 (invoke-method i method-name 1)))
                        (fn minus-one? [i]  (= -1 (invoke-method i method-name -1)))
                        (fn is-zero? [i]  (= 0 (invoke-method i method-name 0)))
@@ -19,7 +23,7 @@
   	(defn -main []
      (time
        (dorun
-         (superoptimise-pmap 6 class-name method-name method-signature eq-tests-filter))))
+        (superoptimise-pmap 6 class-name method-name method-signature eq-tests-filter))))
 
     (defn run-slice
       "Superoptimises a small slice of the overall search space"
