41d80696b9e87cfdd611184b7e7af6d6ffd03f01
Tom Hume <twhume@gmail.com>
2012-08-17 12:10:52 +0100

    Filter performance tests

--- SuperOptimiser/project.clj
+++ SuperOptimiser/project.clj
@@ -7,5 +7,6 @@
                  [noir "1.3.0-beta1"]
                  [clj-http "0.5.2"]
                  [org.clojure/tools.logging "0.2.3"]
+                 [clojure-csv/clojure-csv "2.0.0-alpha1"]
                  [org.clojure/math.combinatorics "0.0.2"]
                  [org.clojure/data.priority-map "0.0.1"]])
--- SuperOptimiser/src/Tests/PruningTests.clj
+++ SuperOptimiser/src/Tests/PruningTests.clj
@@ -1,20 +1,30 @@
 (ns Tests.PruningTests)
 (use 'Main.Opcodes)
+(use 'Main.Global)
+(use 'clojure-csv.core)
+(use 'clojure.math.combinatorics)
 
 ; Lets us measure the effectiveness of the pruning strategies we have in
 ; place, by running Monte Carlo-style tests; pick a random fertile sequence
 ; of the length we're testing, see how many children it has with our current
 ; pruning strategy, and repeat.
 
-(use '[org.tomhume.so.Opcodes])
+(use '[Filters.RedundancyFilter :only (no-redundancy?)])
+(use '[Filters.InfluenceFilter :only (retains-influence?)])
+(use '[Filters.OperandStackFilter :only (uses-operand-stack-ok?)])
+(use '[Filters.VariableUseFilter :only (uses-vars-ok?)])
+(use '[Filters.ReturnFilter :only (finishes-ireturn? no-ireturn?)])
+(use '[Filters.StackHeightFilter :only (branches-respect-stack-height?)])
 
 (def fertility-filter (partial is-fertile? 1))
+
+(defn gc [n s] (map #(conj s (list %)) (keys opcodes)))
 
 (defn rand-opcode-sequence
   "Returns a single valid opcode sequence, randomly generated, of length n"
   [n]
   (loop [depth 0 s '[]]
-    (let [fertile-children (get-children 1 s) ]
+    (let [fertile-children (gc 1 s) ]
     (if (= depth n) s
       (recur (inc depth) (nth fertile-children (rand-int (count fertile-children))))))))
 
@@ -33,3 +43,60 @@
   [n]
   (float ( / (reduce * (prune-stats n))
              (reduce * (repeat n (count (keys opcodes)))))))
+
+(defn single-sequence-filter-performance
+  "Prints a line of each filter's act on the supplied sequence"
+  [filters sequence]
+  (map #(if (% sequence) 0 1) filters))
+
+(defn get-combinations ""
+  [fields-in data-in]
+  (loop [fields fields-in
+          data data-in
+          entries '()]
+     (let [field (first fields)
+           item (first data)]
+       (if (empty? fields) entries
+         (recur (rest fields) (rest data) (if (= 0 item) entries (conj entries (name field))))))))
+
+(defn filter-performance
+  "Creates a dump of filter performance"
+  [seq-length num-args num-tests]
+  (let [filters [(partial retains-influence? num-args)
+                 uses-operand-stack-ok?
+                 (partial uses-vars-ok? num-args true)
+                 finishes-ireturn?
+                 (partial no-redundancy? num-args)]
+        single-fn (partial single-sequence-filter-performance filters)]
+         (map single-fn (repeatedly num-tests (partial rand-opcode-sequence seq-length)))))
+ 
+(defn inc-key
+  "Increments the value of the key in the hash, sets it to 1 if it doesn't already exist"
+  [h k]
+;  (println "inc-key" h k)
+  (assoc h k (if (contains? h k) (inc (get h k)) 1)))
+
+(defn update-stats
+  "Updates the supplied hash to increment the count for all values in the supplied list"
+  [h l]
+;  (println "update-stats" h l)
+  (loop [hsh h
+         remainder l]
+    (let [key (first remainder)]
+      (if (empty? remainder) hsh
+        (recur (inc-key hsh key) (rest remainder))))))
+
+(defn filter-stats
+  ""
+  [seq-length num-args num-tests]
+       (map subsets
+            (map (partial get-combinations  '(influence opstack vars return redundancy))
+                 (filter-performance seq-length num-args num-tests))))
+
+(defn filter-report
+  ""
+  [seq-length num-args num-tests]
+  (loop [h {}
+         remainder (filter-stats seq-length num-args num-tests)]
+    (if (empty? remainder) h
+      (recur (update-stats h (first remainder)) (rest remainder)))))
