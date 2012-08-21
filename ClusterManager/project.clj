2dd147e03f660f64fb8403fd9d8c14d7801ed2ee
Tom Hume <twhume@gmail.com>
2012-08-21 17:04:18 +0100

    Removed old ClusterManager project

--- ClusterManager/project.clj
+++
@@ -1,4 +0,0 @@
-(defproject ClusterManager "0.0.1"
-  :description "Manages the parallelisation of Superoptimiser jobs across many nodes"
-  :dependencies [[org.clojure/clojure "1.4.0"]
-                 [lein-eclipse "1.0.0"]])
